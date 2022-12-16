69879c38edbfd3561e09ce3e34b3a5f9bf278222
Prabhatlodhi <lodhi.prabhat@gmail.com>
2022-12-16 23:22:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+let myObj = {
+    name: "prabhat",
+    mobile:94070
+}
+
+// let aa = JSON.stringify(myObj);
+
+// localStorage.setItem('keys', aa);
+
+// console.log(aa); // 6-12-2022
+
+
+// let aa = localStorage.setItem('keys', JSON.stringify(myObj));
+
+// console.log(aa) WORK NAHI KAREGA CONSOLE NAHI HOGA
+
+
+
+// let bb = JSON.parse(localStorage.getItem('myObj'));
+
+// console.log(bb)
+
+// let storing = JSON.stringify(myObj);
+// localStorage.setItem('keys', storing);
+
+
+// console.log(storing);
+
+
+// let giving = JSON.parse(localStorage.getItem('myObj'));
+// console.log(giving);
+
+
+
+// let storing = JSON.stringify(myObj);
+
+// localStorage.setItem('tasks', storing);
+
+
+// let giving = JSON.parse(localStorage.getItem('myObj'));
+
+// console.log(storing);
+// console.log(giving);
+
+
+// let aa = localStorage.setItem('myObj', JSON.stringify(myObj));
+// let yy = JSON.parse(localStorage.getItem('myObj'));
+// console.log(yy); 
+
+
+
